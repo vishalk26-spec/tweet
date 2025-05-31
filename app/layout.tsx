ff6f4cb56d21fc9107cd8125bb2402c7e3879ab4
VISHAL <vishalkumargeed@gmail.com>
2025-05-31 20:27:23 +0000

    fixed font issue
    Signed-off-by: VISHAL <vishalkumargeed@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,13 @@
 import type { Metadata } from "next";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Idea Bird",
-  description: "Generate engaging tweets for your business/ personal brand",
-
-
+  title: "IdeaBird - AI Tweet Generator",
+  description: "Generate engaging tweets with AI",
 };
 
 export default function RootLayout({
@@ -34,25 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/1024px/1f426.png" />
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
-            <SignInButton />
-            <SignUpButton />
-          </SignedOut>
-          <SignedIn>
-            <div className="flex items-center gap-4 text-5xl">
-            <UserButton />
-            </div>
-            
-          </SignedIn>
-        </header>
-        {children}
-      </body>
-    </html>
-  </ClerkProvider>
-
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
