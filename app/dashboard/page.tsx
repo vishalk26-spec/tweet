e131b6655190b488bec1d35d7ed769553ab26742
VISHAL <vishalkumargeed@gmail.com>
2025-05-31 18:34:03 +0000

    added user authentication on get started

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,14 @@
-'use client'
-
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
-
-  const router = useRouter();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <h1 className="text-8xl font-bold">Idea Bird 🕊️</h1>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md mx-auto" onClick={() => router.push("/dashboard")} >Generate Engaging Tweets </button>
+      <h1 className="text-8xl font-bold">WElcome</h1>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md mx-auto"  >Generate Engaging Tweets </button>
 
-    
+      <Link href={"/dashboard"}></Link>
       </main>
    
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -25,13 +33,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
         {children}
       </body>
     </html>
+  </ClerkProvider>
+
   );
 }
