d208361fbb5dbeaf50ae6f44a0a83b4ec45697a4
VISHAL <vishalkumargeed@gmail.com>
2025-05-31 20:24:30 +0000

    changed next.config.js file
    Signed-off-by: VISHAL <vishalkumargeed@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['img.clerk.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
+
+module.exports = nextConfig 
